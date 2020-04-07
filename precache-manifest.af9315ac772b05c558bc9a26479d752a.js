2edce4486f0901bade021651f77faf9e1ea9592a
Manasa Mandadapu, M <m.manasa.mandadapu@accenture.com>
2020-04-08 01:34:37 +0530

    Updates

---
+++ precache-manifest.af9315ac772b05c558bc9a26479d752a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e8b876f1377167179404526c6c1c52a",
+    "url": "/app-material/index.html"
+  },
+  {
+    "revision": "08775a6f3acac356788a",
+    "url": "/app-material/static/css/main.b3ddc96e.chunk.css"
+  },
+  {
+    "revision": "70b0bae21384bc1552b7",
+    "url": "/app-material/static/js/2.d52edec4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/app-material/static/js/2.d52edec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08775a6f3acac356788a",
+    "url": "/app-material/static/js/main.4eb866ad.chunk.js"
+  },
+  {
+    "revision": "957206061d44a00bfc38",
+    "url": "/app-material/static/js/runtime-main.8029984d.js"
+  }
+]);
